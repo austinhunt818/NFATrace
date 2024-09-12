9effea1e8d14371db80c92732795ed109dbd3744
KyleHenning2 <kylehenning@u.boisestate.edu>
2024-09-12 14:03:33 -0600

    Initial commit

---
+++ NFATrace.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NFATrace {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("NFATrace");
+        frame.setLayout(new BorderLayout());
+
+        JLabel label = new JLabel("Input the transition table of the NFA");
+        frame.add(label, BorderLayout.NORTH);
+
+        DefaultTableModel model = new DefaultTableModel(new String[]{"From", "To"}, 0);
+        JTable table = new JTable(model);
+        table.setGridColor(Color.BLACK);
+        
+        JScrollPane scrollPane = new JScrollPane(table);
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        JButton addRowButton = new JButton("Add Row");
+        JButton submitButton = new JButton("Submit");
+        buttonPanel.add(addRowButton);
+        buttonPanel.add(submitButton);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        addRowButton.addActionListener(e -> model.addRow(new Object[]{"", ""}));
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StringBuilder result = new StringBuilder("Transitions:\n");
+                for (int i = 0; i < model.getRowCount(); i++) {
+                    String from = (String) model.getValueAt(i, 0);
+                    String to = (String) model.getValueAt(i, 1);
+                    result.append("From: ").append(from).append(", To: ").append(to).append("\n");
+                }
+                JOptionPane.showMessageDialog(frame, result.toString(), "Input Data", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+}
